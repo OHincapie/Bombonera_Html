37ff0bc5c054ac486ab640da34e5315588cfc195
David02 <hincapied07@gmail.com>
2022-02-12 10:21:25 -0500

    first commit

---
+++ src/app/antiguo/antiguo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-antiguo',
+  templateUrl: './antiguo.component.html',
+  styleUrls: ['./antiguo.component.scss']
+})
+export class AntiguoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
